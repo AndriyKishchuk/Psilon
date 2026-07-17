a6739b81a146201314ca44f6c9fbe2fb860562f7
Andrii Kishchuk <andriy302007@gmail.com>
2026-07-17 09:07:23 +0200

    Add internship student portal module

---
+++ psi_internship_portal/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Internship Student Portal",
+    "version": "18.0.1.0.0",
+    "summary": "Portal for internship students",
+    "category": "Website",
+    "depends": [
+        "website",
+        "portal",
+        "psi_internship_management",
+    ],
+    "data": [
+        "views/portal_templates.xml",
+    ],
+    "license": "LGPL-3",
+    "installable": True,
+    "application": True,
+}
---
+++ psi_internship_portal/controllers/main.py
@@ -0,0 +1,34 @@
+from odoo import http
+from odoo.http import request
+
+
+class InternshipPortalController(http.Controller):
+
+    @http.route("/my/internship", type="http", auth="user", website=True)
+    def my_internship(self, **kwargs):
+        student = request.env["psi.intern.student"].sudo().search(
+            [("user_id", "=", request.env.user.id)],
+            limit=1,
+        )
+
+        values = {
+            "student": student,
+            "page_name": "my_internship",
+        }
+        return request.render("psi_internship_portal.portal_my_internship", values)
+
+    @http.route("/my/internship/attendance", type="http", auth="user", website=True)
+    def my_internship_attendance(self, **kwargs):
+        student = request.env["psi.intern.student"].sudo().search(
+            [("user_id", "=", request.env.user.id)],
+            limit=1,
+        )
+
+        attendances = student.attendance_ids.sorted("start_datetime") if student else request.env["psi.intern.attendance"]
+
+        values = {
+            "student": student,
+            "attendances": attendances,
+            "page_name": "my_internship_attendance",
+        }
+        return request.render("psi_internship_portal.portal_my_internship_attendance", values)
